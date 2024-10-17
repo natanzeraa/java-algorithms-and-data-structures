cb639244ec33278f6b885b97569b0ae3f0f5c6e0
natanzeraa <natanzeraaaaaa@gmail.com>
2024-10-17 14:11:42 -0300

    first commit

--- src/main/java/org/learnjava/twosum/TwoSum.java
+++ src/main/java/org/learnjava/twosum/TwoSum.java
@@ -1,4 +1,26 @@
 package org.learnjava.twosum;
 
+import java.util.*;
+
+/**
+ * You can find this challenge at: https://leetcode.com/problems/two-sum/description/
+ * Please don't forget to check out the tests file.
+ */
+
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer complementIndex = hash.get(nums[i]);
+
+            if (complementIndex != null) {
+                return new int[]{complementIndex, i};
+            }
+
+            hash.put(target - nums[i], i);
+        }
+        return nums;
+    }
 }
+
--- src/test/java/org/learnjava/twosum/TwoSumTest.java
+++ src/test/java/org/learnjava/twosum/TwoSumTest.java
@@ -1,4 +1,27 @@
+package org.learnjava.twosum;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoSumTest {
-  
+
+    @Test
+    void shouldReturnAnArrayWithPositionsZeroAndOne() {
+        var twoSum = new TwoSum();
+        assertArrayEquals(new int[]{0, 1}, twoSum.twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+
+    @Test
+    void shouldReturnAnArrayWithPositionsOneAndTwo() {
+        var twoSum = new TwoSum();
+        assertArrayEquals(new int[]{1, 2}, twoSum.twoSum(new int[]{3, 2, 4}, 6));
+    }
+
+    @Test
+    void shouldReturnAnArrayWithPositionsTwoAndFour() {
+        var twoSum = new TwoSum();
+        assertArrayEquals(new int[]{2, 4}, twoSum.twoSum(new int[]{24, 7, 2, 15, 6, 3}, 8));
+    }
+
 }
